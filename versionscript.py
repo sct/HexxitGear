b2e4756e946063b1b4e5ef8386dfc82e226010ba
sct <sctsnipe@gmail.com>
2013-06-17 01:52:53 -0400

    Added build script

---
+++ versionscript.py
@@ -0,0 +1,27 @@
+import sys
+import json
+
+argversion = sys.argv
+
+versionlist = argversion[1].split('R')
+
+mcversion = versionlist[0]
+modversion = versionlist[1]
+if(len(versionlist) > 2):
+    modversion = versionlist[1]+"R"+versionlist[2]
+
+mcmod = open('mcmod.info', 'r')
+
+data = json.load(mcmod)
+
+for mod in data['modlist']:
+    for attribute, value in mod.iteritems():
+        if (attribute == 'version'):
+            mod[attribute] = modversion
+        if (attribute == 'mcversion'):
+            mod[attribute] = mcversion
+
+mcmod = open('mcmod.info', 'w')
+json.dump(data, mcmod, indent=4)
+
+print (modversion)
